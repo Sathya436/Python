cc96e771f248656d497c84d92bbe44c1bb26e35b
sathya <sathyanarayanan436@gmail.com>
2022-11-04 23:12:17 +0530

    Day 2

--- Basics/Hello_python.py
+++ Basics/Hello_python.py
@@ -1,2 +1,27 @@
+"""
+# Input
 a = input("Enter string")
+b = int(input("Enter num")
+#python standard declaration of variable "is ref variable space=datatype(input (object))"
 print(a)
+print(b)
+"""
+"""
+# Need for type casting
+a = input("Enter a")
+b = input("Enter b")
+# python always treat its input as string c=a+b is invalid
+# so we have to type cast
+c =int(a)+int(b)
+print(c)
+"""
+
+"""
+#problem add float with int
+a = float(input("Enter float a"))
+b = int(input("Enter int b"))
+c = float(a) + float(b)
+#Converting into the highest preceding operator and adding
+print("Sum is" + " " + str(c))
+# python doesn't know to concat str with int,float ect so convert into str
+"""
